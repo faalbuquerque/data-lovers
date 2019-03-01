4564285ce58ca7784cb2029494641c17ea780a38
Fabiane Albuquerque <bii.nett@gmail.com>
2019-03-01 14:19:03 -0300

    Criei css e crei funcoes para o botao

--- src/main.js
+++ src/main.js
@@ -1,6 +1,36 @@
 window.onload= function(){
     showNamesPokemon()
+    selectedPokemon()
+    showTypePokemon(selectType);
+    showTypePokemon(selectWeaknesses);
 }
+//Parte do codigo para esconder as telas de acordo com o select, falta fazer os botoes!
+
+document.querySelector("#btn-voltar").style.display = "none";
+
+function hideScreenName(){
+    document.querySelector("#section-types").style.display = "none";
+    document.querySelector("#section-weaknesses").style.display = "none";
+    document.querySelector(".imagem-box").style.display = "none";
+    document.querySelector(".text-select-box").style.display = "none";
+    document.querySelector("#btn-voltar").style.display = "block";
+}
+
+
+function hideScreenType(){
+    document.querySelector("#section-names").style.display = "none";
+    document.querySelector("#section-weaknesses").style.display = "none";
+  
+}
+
+function hideScreenWeaknesses(){
+    document.querySelector("#section-names").style.display = "none";
+    document.querySelector("#section-types").style.display = "none";
+    
+    
+}
+//Acaba aqui
+
 
 function getNamesPokemon(){
     return POKEMON["pokemon"]
@@ -8,7 +38,7 @@ function getNamesPokemon(){
 
 function showNamesPokemon(){
     let namesPokemon= document.getElementById("names-pokemon")
-    namesPokemon.innerHTML+= `
+    namesPokemon.innerHTML += `
     ${getNamesPokemon().map((names)=> `
         <option value="${names['id']}" class="list-pokemon">
              ${names['name']}
@@ -18,17 +48,17 @@ function showNamesPokemon(){
 }
 
 function selectedPokemon(){
-    let pokemoneEl= document.getElementById("names-pokemon")
-    let pokemonId= pokemoneEl.options[pokemoneEl.selectedIndex].value
+    let pokemoneEl = document.getElementById("names-pokemon")
+    let pokemonId = pokemoneEl.options[pokemoneEl.selectedIndex].value;
   
-    let result= POKEMON['pokemon'].filter(pokemon => pokemon.id == pokemonId)
+    let result = POKEMON['pokemon'].filter(pokemon => pokemon.id == pokemonId);
 
-    let showPokemon= document.getElementById('get-pokemon')
-    showPokemon.innerHTML= ''
-    showPokemon.innerHTML+= `
+    let showPokemon = document.getElementById("display-name");
+    showPokemon.innerHTML = ''
+    showPokemon.innerHTML += `
         ${result.map( pokemon => `
-            <p>Nome: ${pokemon.name}</p>
             <img src="${pokemon.img}">
+            <p>Nome: ${pokemon.name}</p>
             <p> Tipo: ${pokemon.type}</p>
             <p>Altura: ${pokemon.height}</p>
             <p>Peso: ${pokemon.weight}</p>
@@ -42,64 +72,72 @@ function selectedPokemon(){
             <p>Fraquezas: ${pokemon.weaknesses}</p>
             <p>Proxima evolucao: ${pokemon.next_evolution}</p>
         `).join("")}
-    `
+        `
 }
 
-let selectType = document.getElementById("select-type");
-let displayType = document.getElementById("display-type");
-selectType.addEventListener("change", showTypePokemon);
+let selectType = document.querySelector("#select-type");
+let displayType = document.querySelector("#display-type");
+let selectWeaknesses = document.querySelector("#select-weaknesses");
+let displayWeaknesses = document.querySelector("#display-weaknesses");
+let arrayTypes = ["Grass", "Fire", "Water", "Bug", "Normal", "Poison", "Electric", "Ground", "Fighting", "Psychic", "Rock", "Flying", "Ghost", "Ice", "Dragon", "Steel", "Dark", "Fairy"];
 
-function showTypePokemon(){
-    let type = selectType.value;
-    displayType.innerHTML = "";
-    for (datas in POKEMON["pokemon"]){
-        for (i in POKEMON["pokemon"][datas]["type"]){
-            if (type === POKEMON["pokemon"][datas]["type"][i]){
-                mostrar();      
-            } 
-        }  
-    }      
+function showTypePokemon(category){
+    for(i in arrayTypes){
+        category.innerHTML += `
+            <option value="${arrayTypes[i]}" class="list-pokemon">
+             ${arrayTypes[i]}
+            </option>    
+        `
+    }
 }
 
-function mostrar(){
-    displayType.innerHTML += `
-    <section class="pokemons-type">
-        <div class="pokemon-type">
-            <img src="${POKEMON["pokemon"][datas]["img"]}" class="poke-photo" />   
-            <div class="text-name">
-                <h3 class="poke-name">${POKEMON["pokemon"][datas]["name"]}</h3>
-            </div>
-            <div class="text-type">
-                <p class="poke-type"> Tipo: ${POKEMON["pokemon"][datas]["type"].join(", ")}</p>
-            </div>
-        </div> 
-    </section>
-`
+selectType.addEventListener("change", () => {
+selectedPokemonFrom('type', selectType, displayType);
+});
+
+selectWeaknesses.addEventListener("change", () => {
+selectedPokemonFrom('weaknesses', selectWeaknesses, displayWeaknesses);
+});
+
+function selectedPokemonFrom(categorySelect, dataSelect, displayTag){
+    displayTag.innerHTML = "";
+    let pokemonsFromType = POKEMON.pokemon.filter(
+        (pokemon) => {
+            let typeFilter = pokemon[categorySelect].filter(
+                (type) => {
+                    return type === dataSelect.value;
+                }
+            );
+            if(typeFilter.length > 0){
+                showPokemon(pokemon, displayTag);
+                return true;
+            }
+        }
+    );
+    if (pokemonsFromType.length === 0) {
+        displayTag.innerHTML += `
+        <p> Nenhum pokémon encontrado </p>
+        `
+    }
 }
 
-function orderNome(){
 
-    let teste= getNamesPokemon().value
+function showPokemon(pokemon, tagById){
+    let nextEvolution = pokemon["next_evolution"] ? pokemon["next_evolution"][0].name : 'Sem evolução';
 
-    const list= document.querySelector(".list-pokemon")
-    orderArray.sort(function(a,b) {
-        if ( a.name < b.name )
-            return -1;
-        if ( a.name > b.name )
-            return 1;
-        return 0;
-    } );  
-    
-    console.log(teste)
+    tagById.innerHTML += `
+                <section class="pokemons-type">
+                    <div class="pokemon-type">
+                        <img src="${pokemon.img}" class="poke-photo" />   
+                        <div class="text-name">
+                            <h3 class="poke-name">${pokemon.name}</h3>
+                        </div>
+                        <div class="text-type">
+                            <p class="poke-type"> Tipo: ${pokemon.type.join(", ")}</p>
+                            <p> Fraquezas: ${pokemon.weaknesses.join(", ")}</p>
+                            <p> Próxima(s) Evolução(ões): ${nextEvolution}</p>
+                        </div>
+                    </div> 
+                </section>        
+                `
 }
-
-
-/*
-function orderNome() {
-    const list= document.querySelector(".list-pokemon");
-    const listChildren= [...list.children]
-        .map(item => item.textContent).sort();
-
-       list.innerHTML= listChildren.map(item => `<p>${item}</p>`).join('');
-  }
-*/
