bc664f212a5e71fdd1aa83238b548e9d839e0c6b
Vanessa Yoshida <vanessayoshida92@gmail.com>
2019-03-07 08:27:21 -0300

    feito função para order por nome de A-Z, de Z-A ou por ID

--- src/main.js
+++ src/main.js
@@ -1,33 +1,40 @@
 window.onload= function(){
     showNamesPokemon()
     selectedPokemon()
-    showTypePokemon(selectType);
-    showTypePokemon(selectWeaknesses);
+    showTypePokemon(selectType)
+    showTypePokemon(selectWeaknesses)
 }
-//Parte do codigo para esconder as telas de acordo com o select, falta fazer os botoes!
 
-document.querySelector("#btn-voltar").style.display = "none";
+//Parte do codigo para esconder as telas de acordo com o select, falta fazer os botoes!
+let selectType = document.querySelector("#select-type")
+let displayType = document.querySelector("#display-type")
+let selectWeaknesses = document.querySelector("#select-weaknesses")
+let displayWeaknesses = document.querySelector("#display-weaknesses")
+let order = document.querySelector("#order")
+document.querySelector("#btn-exit-name").style.display = "none"
 
 function hideScreenName(){
-    document.querySelector("#section-types").style.display = "none";
-    document.querySelector("#section-weaknesses").style.display = "none";
-    document.querySelector(".imagem-box").style.display = "none";
-    document.querySelector(".text-select-box").style.display = "none";
-    document.querySelector("#btn-voltar").style.display = "block";
+    document.querySelector("#section-types").style.display = "none"
+    document.querySelector("#section-weaknesses").style.display = "none"
+    document.querySelector(".imagem-box").style.display = "none"
+    document.querySelector(".text-select-box").style.display = "none"
+    document.querySelector("#btn-exit-name").style.display = "block"
 }
 
-
 function hideScreenType(){
-    document.querySelector("#section-names").style.display = "none";
-    document.querySelector("#section-weaknesses").style.display = "none";
-  
+    document.querySelector("#show-type").classList.remove("hide")
+    document.querySelector("#order").classList.remove("hide")
+    document.querySelector("#img-type").classList.add("hide")   
+    document.querySelector("#text-type").classList.add("hide") 
+    document.querySelector("#section-names").classList.add("hide") 
+    document.querySelector("#section-weaknesses").classList.add("hide")   
 }
 
 function hideScreenWeaknesses(){
-    document.querySelector("#section-names").style.display = "none";
-    document.querySelector("#section-types").style.display = "none";
-    
-    
+    document.querySelector("#section-names").style.display = "none"
+    document.querySelector("#section-types").style.display = "none"
+    document.querySelector("#img-weaknesses").classList.add("hide")   
+    document.querySelector("#text-weaknesses").classList.add("hide") 
 }
 //Acaba aqui
 
@@ -45,7 +52,7 @@ function getOrderedPokemonByNames(){
 }
 
 function showNamesPokemon(){
-    let namesPokemon= document.getElementById("names-pokemon")
+    let namesPokemon= document.querySelector("#names-pokemon")
 
     namesPokemon.innerHTML += `
     ${getOrderedPokemonByNames().map((pokemon)=> `
@@ -58,18 +65,21 @@ function showNamesPokemon(){
 }
 
 function selectedPokemon(){
-    let pokemoneEl = document.getElementById("names-pokemon")
-    let pokemonId = pokemoneEl.options[pokemoneEl.selectedIndex].value;
+    let pokemoneEl = document.querySelector("#names-pokemon")
+    let pokemonId = pokemoneEl.options[pokemoneEl.selectedIndex].value
   
-    let result = POKEMON['pokemon'].filter(pokemon => pokemon.id == pokemonId);
+    let result = POKEMON['pokemon'].filter(pokemon => pokemon.id == pokemonId)
 
-    let showPokemon = document.getElementById("display-name");
+    let nextEvolution = POKEMON.pokemon["next_evolution"] ? POKEMON.pokemon["next_evolution"][0].name : 'Sem evolução'
+    
+    let showPokemon = document.querySelector("#display-name")
     showPokemon.innerHTML = ''
     showPokemon.innerHTML += `
         ${result.map( pokemon => `
             <img src="${pokemon.img}">
             <p>Nome: ${pokemon.name}</p>
-            <p> Tipo: ${pokemon.type}</p>
+            <p>Nº Pokedex: #${pokemon.num}</p>
+            <p>Tipo: ${pokemon.type}</p>
             <p>Altura: ${pokemon.height}</p>
             <p>Peso: ${pokemon.weight}</p>
             <p>Candy: ${pokemon.candy}</p>
@@ -80,48 +90,57 @@ function selectedPokemon(){
             <p>Encontrar jogadores: ${pokemon.spawn_time}</p>
             <p>Multiplicadores: ${pokemon.multipliers}</p>
             <p>Fraquezas: ${pokemon.weaknesses}</p>
-            <p>Proxima evolucao: ${pokemon.next_evolution}</p>
+            <p>Proxima evolucao: ${nextEvolution}</p>
         `).join("")}
         `
 }
 
-let selectType = document.querySelector("#select-type");
-let displayType = document.querySelector("#display-type");
-let selectWeaknesses = document.querySelector("#select-weaknesses");
-let displayWeaknesses = document.querySelector("#display-weaknesses");
-let arrayTypes = ["Grass", "Fire", "Water", "Bug", "Normal", "Poison", "Electric", "Ground", "Fighting", "Psychic", "Rock", "Flying", "Ghost", "Ice", "Dragon", "Steel", "Dark", "Fairy"];
-arrayTypes.sort();
+let arrayType = [];
+
+POKEMON.pokemon.map((pokemon) => {
+    pokemon.type.map((type) => {
+        if(!arrayType.includes(type)){
+            arrayType.push(type);
+        }
+    })
+})
 
 function showTypePokemon(category){
-    for(i in arrayTypes){
+    for(i in arrayType){
         category.innerHTML += `
-            <option value="${arrayTypes[i]}" class="list-pokemon">
-             ${arrayTypes[i]}
+            <option value="${arrayType[i]}" class="list-pokemon">
+             ${arrayType[i]}
             </option>    
         `
     }
 }
 
+var sectionFilterType, sectionFilterWeaknesses, categorySectionFilter;
+
+
 selectType.addEventListener("change", () => {
-selectedPokemonFrom('type', selectType, displayType);
+sectionFilterType = selectedPokemonFrom('type', selectType, displayType)
+categorySectionFilter = "type"
+hideScreenType()
 });
 
 selectWeaknesses.addEventListener("change", () => {
-selectedPokemonFrom('weaknesses', selectWeaknesses, displayWeaknesses);
-});
+    sectionFilterWeaknesses = selectedPokemonFrom('weaknesses', selectWeaknesses, displayWeaknesses)
+    categorySectionFilter = "weaknesses"
+})
 
 function selectedPokemonFrom(categorySelect, dataSelect, displayTag){
-    displayTag.innerHTML = "";
+    displayTag.innerHTML = ""
     let pokemonsFromType = POKEMON.pokemon.filter(
         (pokemon) => {
             let typeFilter = pokemon[categorySelect].filter(
                 (type) => {
-                    return type === dataSelect.value;
+                    return type === dataSelect.value
                 }
             );
             if(typeFilter.length > 0){
-                showPokemon(pokemon, displayTag);
-                return true;
+                showPokemon(pokemon, displayTag)
+                return true
             }
         }
     );
@@ -130,11 +149,12 @@ function selectedPokemonFrom(categorySelect, dataSelect, displayTag){
         <p> Nenhum pokémon encontrado </p>
         `
     }
+    return pokemonsFromType;
 }
 
 
 function showPokemon(pokemon, tagById){
-    let nextEvolution = pokemon["next_evolution"] ? pokemon["next_evolution"][0].name : 'Sem evolução';
+    let nextEvolution = pokemon["next_evolution"] ? pokemon["next_evolution"][0].name : 'Sem evolução'
 
     tagById.innerHTML += `
                 <section class="pokemons-type">
@@ -144,7 +164,10 @@ function showPokemon(pokemon, tagById){
                             <h3 class="poke-name">${pokemon.name}</h3>
                         </div>
                         <div class="text-type">
+                            <p> Nº Pokedex: #${pokemon.num}</p>
                             <p class="poke-type"> Tipo: ${pokemon.type.join(", ")}</p>
+                            <p> Qtd de Candys para evoluir: ${pokemon.candy_count}</p>
+                            <p>Chance de encontrar: ${pokemon.spawn_chance}</p>
                             <p> Fraquezas: ${pokemon.weaknesses.join(", ")}</p>
                             <p> Próxima(s) Evolução(ões): ${nextEvolution}</p>
                         </div>
@@ -152,3 +175,69 @@ function showPokemon(pokemon, tagById){
                 </section>        
                 `
 }
+
+order.addEventListener("change", () => {
+    let arrayOrder;
+    if (categorySectionFilter === "type"){
+        arrayOrder = orderBy(order.value, sectionFilterType)
+    }
+    else if (categorySectionFilter === "weaknesses"){
+        arrayOrder = orderBy(order.value, sectionFilterWeaknesses)
+    }
+    displayType.innerHTML = ""
+    displayWeaknesses.innerHTML = ""
+    for (item of arrayOrder){
+        if (categorySectionFilter === "type"){
+            showPokemon(item, displayType)
+        }
+        else if (categorySectionFilter === "weaknesses"){
+            showPokemon(item, displayWeaknesses)
+        }
+    }
+    });
+
+function orderBy(choiceOrder, section){
+    let choice = choiceOrder;
+
+    if (choice === "name-A-Z"){
+        return section.sort((a, b) => {
+            if (a.name > b.name){
+                return 1;
+            }
+            if (a.name < b.name){
+                return -1;
+            }
+            return 0;
+        });
+    }
+    else if (choice === "name-Z-A"){
+        return section.sort((a, b) => {
+            if (a.name < b.name){
+                return 1;
+            }
+            if (a.name > b.name){
+                return -1;
+            }
+            return 0;
+        });
+    }
+    else if (choice === "id"){
+        return section.sort((a, b) => {
+        if (a.id > b.id){
+            return 1;
+        }
+        if (a.id < b.id){
+            return -1;
+        }
+        return 0;
+    });
+    }
+}
+
+let btnBack = document.querySelector("#btn-exit-name")
+btnBack.addEventListener("click", function(){
+    document.location.reload(true)
+    });
+
+
+
